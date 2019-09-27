94537443212bf693c36c4c970eebdad1462e733d
David Mai <40285081+dmai261@users.noreply.github.com>
2019-09-27 01:30:13 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+const PORT = '8080'
+
+app.use(cors());
+
+app.get('/words', function(req, res, next) {
+  axios.get('http://app.linkedin-reach.io/words')
+    .then((response)=> {
+      const data = response.data; 
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    })
+})
+
+app.listen(PORT, function() {
+  console.log('listening on port ' + PORT);
+});
